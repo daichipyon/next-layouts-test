292722491f63672ec51b41391b11666395cc355d
SoraKumo <info@croud.jp>
2022-10-30 20:21:49 +0900

    first-commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+const Page = () => {
+  return (
+    <>
+      <div>
+        <a href="/suspense">Suspense有り</a>
+      </div>
+      <div>
+        <a href="/nosuspense">Suspense無し</a>
+      </div>
+    </>
+  );
+};
+export default Page;
---
+++ src/app/suspense/page.tsx
@@ -0,0 +1,48 @@
+import React, { Suspense, use } from 'react';
+
+export interface WeatherType {
+  publishingOffice: string;
+  reportDatetime: string;
+  targetArea: string;
+  headlineText: string;
+  text: string;
+}
+
+const fetchWeather = (id: number): Promise<WeatherType> =>
+  fetch(`https://www.jma.go.jp/bosai/forecast/data/overview_forecast/${id}.json`)
+    .then((r) => r.json())
+    .then(
+      //ウエイト追加
+      (r) => new Promise((resolve) => setTimeout(() => resolve(r), 1000))
+    );
+
+const Weather = () => {
+  //Reactの新機能useでデータを取り出す
+  const weather = use(fetchWeather(130000));
+  return (
+    <div>
+      <h1>{weather.targetArea}</h1>
+      <div>
+        {new Date(weather.reportDatetime).toLocaleString('ja-JP', {
+          timeZone: 'JST',
+          year: 'numeric',
+          month: 'narrow',
+          day: 'numeric',
+          hour: 'numeric',
+          minute: 'numeric',
+        })}
+      </div>
+      <div>{weather.headlineText}</div>
+      <pre>{weather.text}</pre>
+    </div>
+  );
+};
+
+const Page = () => {
+  return (
+    <Suspense fallback={'Loading'}>
+      <Weather />
+    </Suspense>
+  );
+};
+export default Page;
